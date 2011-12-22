f62c9193e050eb6a60c3af0c67ef636c9e69441b
Iain W. Bailey <iainbailey@gmail.com>
2011-12-22 00:38:11 +0000

    changed so we can build

---
+++ setup.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# ***** BEGIN LICENSE BLOCK *****
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+# ***** END LICENSE BLOCK *****
+# setup.py --- 
+# 
+# Filename: setup.py
+# Description: 
+# Author: Iain Bailey
+# Maintainer: 
+# Created: Fri Dec 16 15:10:30 2011 (-0800)
+# Version: 
+# Last-Updated: Tue Dec 20 16:05:12 2011 (-0800)
+#           By: Iain Bailey
+#     Update #: 51
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Change Log:
+# 
+# 
+# 
+# 
+# 
+# 
+
+# Code:
+
+from distutils.core import setup, Extension
+from distutils.sysconfig import get_python_lib, get_config_vars
+import sys, shutil, os, os.path, time, glob, stat
+
+######################################################################
+
+BOOST_BASE = None
+BOOST_LIB = "boost_python"
+BOOST_DLL = "boost_python.dll"
+
+# Name of the entire package (you can't use just this variable to rename
+# the package, all the directories and source files have to be updated
+# accordingly)
+PACKAGE_NAME = "pythmt"
+
+# The following variables are used to customize the compile process
+
+INC_DIRS = []
+LIB_DIRS = []
+LIBS     = []
+CC_ARGS  = []
+LINK_ARGS = []
+MACROS    = []
+data_files = []
+scripts = []
+
+# list of scripts
+scripts = ["scripts/mtRandom.py"
+           , "scripts/mtFaultStyle.py"
+           , "scripts/mtInfo.py"
+           , "scripts/mtBinSum.py"
+           , "scripts/mtDecomp.py"
+           , "scripts/mtSum.py" 
+           , "scripts/psmecaSmtoEig.py"
+            ]
+
+setup( name = PACKAGE_NAME,
+       version = "1.2",
+       description = "Python Moment Tensors",
+       author = "Iain Bailey",
+       author_email = "iainbailey@gmail.com",
+       url = "http://earth.usc.edu/~iwbailey",
+       license = "GPL",
+
+       # Process all pure Python modules in the source directory
+       packages = [PACKAGE_NAME ],
+
+       # Stick all extension modules into the main package
+       ext_package = PACKAGE_NAME,
+       scripts = scripts  )
+
+
+print ("... finished setup")
+
+# 
+# setup.py ends here
